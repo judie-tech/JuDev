d6d07e661732da732b83dac273971c4895ac7bb7
Judy <ritchiejudie691@gmail.com>
2024-12-24 19:50:19 +0300

    updated the meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,34 @@
-import './globals.css'
-import { Inter, Montserrat, Playfair_Display } from 'next/font/google'
+import "./globals.css";
+import { Inter, Montserrat, Playfair_Display } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
-const montserrat = Montserrat({ subsets: ['latin'], variable: '--font-montserrat' })
-const playfair = Playfair_Display({ subsets: ['latin'], variable: '--font-playfair' })
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
+const playfair = Playfair_Display({
+  subsets: ["latin"],
+  variable: "--font-playfair",
+});
 
 export const metadata = {
-  title: 'Your Name - Software Developer',
-  description: 'Personal portfolio of Your Name, a passionate software developer.',
-}
+  title: "JuDev",
+  description:
+    "Personal portfolio of Your Name, a passionate software developer.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${montserrat.variable} ${playfair.variable} font-sans`}>
+      <body
+        className={`${inter.variable} ${montserrat.variable} ${playfair.variable} font-sans`}
+      >
         {children}
       </body>
     </html>
-  )
+  );
 }
-
